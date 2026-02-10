efffce04c5b9664e2289871766d1e36235312ec8
Anton K <ant.korotun@gmail.com>
2026-02-10 14:14:38 +0200

    added columns and filters

--- src/app/components/table/table.ts
+++ src/app/components/table/table.ts
@@ -22,29 +22,80 @@ export class Table {
     {
       name: 'First name',
       sortOrder: null,
-      sortFn: (a: User, b: User) => a.firstName.localeCompare(b.lastName),
+      sortFn: (a: User, b: User) => a.firstName.localeCompare(b.firstName),
       sortDirections: ['ascend', 'descend', null],
-      filterMultiple: true,
+      filterMultiple: false,
       listOfFilter: [],
-      filterFn: (list: string[], item: User) => list.some(name => item.firstName.indexOf(name) !== -1)
+      filterFn: null
     },
     {
       name: 'Last name',
       sortOrder: null,
       sortFn: (a: User, b: User) => a.lastName.localeCompare(b.lastName),
       sortDirections: ['ascend', 'descend', null],
+      filterMultiple: false,
+      listOfFilter: [],
+      filterFn: null
+    },
+    {
+      name: 'Date of birth',
+      sortOrder: null,
+      sortFn: (a: User, b: User) => this.datesCompare(a.birthDate, b.birthDate),
+      sortDirections: ['ascend', 'descend', null],
+      filterMultiple: false,
+      listOfFilter: [],
+      filterFn: null,
+    },
+    {
+      name: 'Title',
+      sortOrder: null,
+      sortFn: (a: User, b: User) => a.title.localeCompare(b.title),
+      sortDirections: ['ascend', 'descend', null],
+      filterMultiple: true,
+      listOfFilter: [
+        { text: 'Mr', value: 'Mr' },
+        { text: 'Mrs', value: 'Mrs'},
+        { text: 'Ms', value: 'Ms'}
+      ],
+      filterFn: (list: string[], item: User) => list.some(name => item.title.indexOf(name) !== -1)
+    },
+    {
+      name: 'Phone number',
+      sortOrder: null,
+      sortFn: null,
+      sortDirections: [null],
       filterMultiple: true,
       listOfFilter: [],
-      filterFn: (list: string[], item: User) => list.some(name => item.lastName.indexOf(name) !== -1)
-    }
+      filterFn: null
+    },
   ]
 
   searchTerm = signal('')
   filteredUsers = computed(()=>{
     const term = this.searchTerm().toLowerCase();
-    return this.users().filter(user =>
-      term === '' || user.firstName.toLowerCase().includes(term))
-  })  
+    return this.users().filter(user => {
+    if (!term) return true;
+
+    const first = user.firstName.toLowerCase();
+    const last = user.lastName.toLowerCase();
+    const full = `${first} ${last}`;
+
+    return (
+      first.includes(term) ||
+      last.includes(term) ||
+      full.includes(term)
+    );
+  });
+  })
+
+  datesCompare(d1: Date, d2: Date){
+    let date1 = new Date(d1)
+    let date2 = new Date(d2)
+    switch(isBefore(date1, date2)){
+      case true: return 1;
+      case false: return -1;
+    }
+  }
 
   ngOnInit(){
     this.userService.getUsers().subscribe({
