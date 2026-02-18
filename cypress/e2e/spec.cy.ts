ea283091770a5c9b02c0e428e397dbcb6370f391
Anton K <ant.korotun@gmail.com>
2026-02-18 16:26:10 +0200

    added tests for counting and some css

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -34,4 +34,57 @@ describe('template spec', () => {
     cy.get('.keyword-input').type('dave')
     cy.get('.nz-disable-td')
   })
+    
+  const allYears: number[] = [];
+  const decades = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let count = [0]
+
+  it('it should display user dates of birth to decades count', ()=>{
+    
+    cy.get('.count-label')
+
+    collectYearsFromPage();
+    cy.then(()=>{
+      for(let i=0; i<decades.length; i++){
+          const usersForEachDecade = allYears.filter(year => {
+          const n1 = year%1000
+          
+          const n2 = Math.floor(n1%100/10)
+          return n2 === decades[i]
+        }
+      )
+      count = [...count, usersForEachDecade.length]
+    }
+    const expectedCounts = count.filter(c => c !== 0);
+     cy.get('.counter')
+     .then($labels => {
+        const actualCounts = [...$labels]
+          .map(el => Number(el.innerText.trim()))
+          .filter(n => n !== 0);
+
+        expect(actualCounts.sort()).to.deep.equal(expectedCounts.sort());
+    });
+  })
+
+  function collectYearsFromPage() {
+    cy.get('[data-cy="birth-date"]') .then(
+      $cells => {
+       const years = [...$cells].map(cell => cell.innerText.trim().slice(0, 4) );
+        allYears.push(...years);
+        console.log(allYears) 
+      })
+      .then(() => {
+        cy.get('.ant-pagination-next > .ant-pagination-item-link').then($btn => {
+          const isDisabled =
+            $btn.prop('disabled') || $btn.hasClass('ant-pagination-disabled');
+
+          if (!isDisabled) {
+            cy.wrap($btn).click();
+            
+            collectYearsFromPage();
+          }
+        });
+      });
+  }
+})
 })
